a894f5cd6681b07fd0105352281aca62996c1fa2
hpa61 <haoming_pan@sfu.ca>
2021-12-10 23:55:17 +0000

    Update create_sql.sql

--- ETL/create_sql.sql
+++ ETL/create_sql.sql
@@ -66,3 +66,66 @@ CREATE TABLE tips (
 COPY tips FROM 's3://c732-hpa61-a5-haomingpan/ProjectYelp/yelp_academic_dataset_tip.json'
 IAM_ROLE 'arn:aws:iam::[Your_IAM_id]:role/RedshiftS3Full'
 FORMAT JSON 'auto';
+
+
+
+
+REATE TABLE high_rate_reataurant_2008 (
+  name VARCHAR(255) PRIMARY KEY,
+  order_numbers int,
+  average_stars float,
+  userful_nums int
+);
+
+COPY high_rate_reataurant_2008 FROM 's3://c732-hpa61-a5-haomingpan/ProjectYelp/High_rate_restaurant/2008'
+IAM_ROLE 'arn:aws:iam::xxxxxxxxx:role/RedshiftS3Full'
+FORMAT JSON 'auto';
+
+
+
+CREATE TABLE high_rate_reataurant_2012 (
+  name VARCHAR(255) PRIMARY KEY,
+  order_numbers int,
+  average_stars float,
+  userful_nums int
+);
+
+COPY high_rate_reataurant_2012 FROM 's3://c732-hpa61-a5-haomingpan/ProjectYelp/High_rate_restaurant/2012'
+IAM_ROLE 'arn:aws:iam::xxxxxxxxx:role/RedshiftS3Full'
+FORMAT JSON 'auto';
+
+
+
+CREATE TABLE high_rate_reataurant_2016 (
+  name VARCHAR(255) PRIMARY KEY,
+  order_numbers int,
+  average_stars float,
+  userful_nums int
+);
+
+COPY high_rate_reataurant_2016 FROM 's3://c732-hpa61-a5-haomingpan/ProjectYelp/High_rate_restaurant/2016'
+IAM_ROLE 'arn:aws:iam::xxxxxxxxx:role/RedshiftS3Full'
+FORMAT JSON 'auto';
+
+
+
+
+CREATE TABLE high_rate_reataurant_2020 (
+  name VARCHAR(255) PRIMARY KEY,
+  order_numbers int,
+  average_stars float,
+  userful_nums int
+);
+
+COPY high_rate_reataurant_2020 FROM 's3://c732-hpa61-a5-haomingpan/ProjectYelp/High_rate_restaurant/2020'
+IAM_ROLE 'arn:aws:iam::xxxxxxxxx:role/RedshiftS3Full'
+FORMAT JSON 'auto';
+
+
+
+# Select all the resaurants survived from 2008 to 2020 in superset
+
+SELECT t1.name
+FROM high_rate_reataurant_2008 t1, high_rate_reataurant_2012 t2, high_rate_reataurant_2016 t3, high_rate_reataurant_2020 t4
+WHERE t1.name = t2.name and t2.name = t3.name and t3.name = t4.name
+
